ebf2e959a4a182332182f59986f0796f412898fa
Mikhail Khlevin <m.khlevin@gmail.com>
2023-02-18 10:16:15 +0400

    initial commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+$test = 2342423234;
+echo $test;
+?>
+
+
+<!DOCTYPE html>
+    <html>
+    <head>
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+        <script src='test.js'></script>
+    </head>
+    <body>
+
+        <input type="text" value="123" class="test"/>
+        <input type="button" value="dsdlkfjsd" onclick="$('.test').focus();"/>
+
+    </body>
+</html>
+
